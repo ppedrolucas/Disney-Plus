5393b9e7ad0c0ef18203630346f8b9474a323cd0
Hematopoese <pp15lucas@gmail.com>
2023-03-07 15:05:34 -0300

    Update for style fonts

--- Backup/login.php
+++ Backup/login.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Disney plus</title>
     <link rel="stylesheet" href="CSS/style-login.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1e32b8079d.js" crossorigin="anonymous"></script>
 </head>
 <body class="banner">
